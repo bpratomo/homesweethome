8f350ba61e2a909c3ca9920ed79061b2cfcca8c1
Budi Pratomo <bpratomo@myrsm.nl>
2019-12-25 17:06:40 +0100

    added uikit

--- scrape_homesweethome/scrape_homesweethome/spiders/pararius_spider.py
+++ scrape_homesweethome/scrape_homesweethome/spiders/pararius_spider.py
@@ -21,7 +21,7 @@ def convert_inclusive_signs_into_booleans(inclusive_sign):
 
     return is_inclusive
                 
-
+page_counter = 0
 
 
 
@@ -37,16 +37,20 @@ class ParariusSpider(scrapy.Spider):
 
     def parse(self, response):
         # #Follow links to each specific house
-        for href in response.xpath("//div[@class='details']/h2/a/@href"):
-            time.sleep(3)
-            print('traversing to house {}'.format(href))
-            yield response.follow(href, self.parse_property)
+        # for href in response.xpath("//div[@class='details']/h2/a/@href"):
+        #     time.sleep(3)
+        #     print('traversing to house {}'.format(href))
+        #     yield response.follow(href, self.parse_property)
 
         # Follow links to next page (maximum 5 pages)
         for href in response.xpath("//li[@class='next']/a/@href"):
-            time.sleep(3)
-            print('traversing to next page {}'.format(href))
-            yield response.follow(href, self.parse)
+            page_number = href.get().split('-')[-1]
+            if page_number == '6': # maximum page number
+                break
+            else:
+                time.sleep(3)
+                print('traversing to next page {}'.format(href))
+                yield response.follow(href, self.parse)
 
 
     def parse_property(self,response):
