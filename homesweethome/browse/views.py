6dcaeb27954abf57af8065176474e8e743cb5d1e
Budi Pratomo <bpratomo@myrsm.nl>
2019-12-26 17:27:39 +0100

    finished landing page

--- homesweethome/browse/views.py
+++ homesweethome/browse/views.py
@@ -6,6 +6,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'browse/index.html')
 
 
